5eec70e9e75e48e230fc006726da2db20791f778
Roller, David <roller@wharton.upenn.edu>
2016-05-26 21:53:00 -0400

    refactored to use pyaml instead of poyo for a little more flexibility

--- cookiecutter/buildit.py
+++ cookiecutter/buildit.py
@@ -1,16 +1,27 @@
-import codecs
-from poyo import parse_string, PoyoException
 from .find import find_build_file
 from .utils import work_in
 from .exceptions import MissingBuildFileParamException
 
+from collections import OrderedDict
 import os
 import pipes
 import subprocess
 import sys
+import yaml
 
 VENV_WRAPPER_PATH = os.environ['VIRTUALENVWRAPPER_SCRIPT']
 VENV_DIR = os.environ['VIRTUALENVWRAPPER_HOOK_DIR']
+
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
 
 def build_project(repo_dir):
     build_file_path = find_build_file(repo_dir)
@@ -18,32 +29,22 @@ def build_project(repo_dir):
     with work_in(repo_dir):
         build_file = os.path.split(build_file_path)[1]
 
-        with open(build_file) as f:
+        with open(build_file, 'r') as f:
             ymldata = f.read()
 
-        data = parse_string(ymldata)
+    build_step_data = ordered_load(ymldata, yaml.SafeLoader)
 
     venv = False
-    if data.get('virtualenv', None):
-        venv = build_virtualenv(data['virtualenv'])
+    if build_step_data.get('virtualenv', None):
+        venv = build_virtualenv(build_step_data['virtualenv'])
+        build_step_data.pop('virtualenv')
 
-    if data.get('requirements', None):
-        build_requirements(repo_dir, data['requirements'], venv)
-
-    if data.get('collectstatic', None):
-        build_collectstatic(repo_dir, venv)
-
-    if data.get('npm', None):
-        build_npm(repo_dir)
-
-    if data.get('bower', None):
-        build_bower(repo_dir)
-
-    if data.get('gulp', None):
-        build_gulp(repo_dir)
-
-    if data.get('apache_config', None):
-        build_apache_config(data['apache_config'], venv)
+    for step in build_step_data:
+        step_name = 'build_' + step
+        if step_name in globals():
+            globals()[step_name](build_step_data[step], repo_dir=repo_dir, venv=venv)
+        else:
+            build_arbitrary(build_step_data[step], repo_dir=repo_dir, venv=venv)
 
 
 def build_virtualenv(data):
@@ -57,14 +58,14 @@ def build_virtualenv(data):
     return venv_name
 
 
-def build_requirements(repo_dir, data, venv):
-    if not venv:
+def build_requirements(data, **kwargs):
+    if not kwargs['venv']:
         raise MissingBuildFileParamException('Cannot install requirements without specifying a virtualenv in the build.yml config')
 
     req_file_path = data['file']
-    venv_name = venv
+    venv_name = kwargs['venv']
 
-    with work_in(repo_dir):
+    with work_in(kwargs['repo_dir']):
         print("\033[93mInstalling project requirements...\033[0m", end='')
         sys.stdout.flush()
         s = subprocess.Popen(['{0}/{1}/bin/pip'.format(VENV_DIR, venv_name), '-q', 'install', '-r', './{0}'.format(req_file_path)])
@@ -72,13 +73,16 @@ def build_requirements(repo_dir, data, venv):
         print("\033[92mFinished\033[0m")
 
 
-def build_collectstatic(repo_dir, venv):
-    if not venv:
+def build_collectstatic(data, **kwargs):
+    if not data:
+        return
+
+    if not kwargs['venv']:
         raise MissingBuildFileParamException("Cannot call collectstatic without specifying a virtualenv in the build.yml config")
 
-    venv_name = venv
+    venv_name = kwargs['venv']
 
-    with work_in(repo_dir):
+    with work_in(kwargs['repo_dir']):
         print("\033[93mRunning collectstatic...\033[0m", end='')
         sys.stdout.flush()
         s = subprocess.Popen(['{0}/{1}/bin/python'.format(VENV_DIR, venv_name), 'manage.py', 'collectstatic', '--noinput', '-v', '0'])
@@ -86,34 +90,42 @@ def build_collectstatic(repo_dir, venv):
         print("\033[92mFinished\033[0m")
 
 
-def build_npm(repo_dir):
+def build_npm(data, **kwargs):
+    if not data:
+        return
+
     print("\033[93mRunning npm install...\033[0m")
 
-    with work_in(repo_dir):
+    with work_in(kwargs['repo_dir']):
         s = subprocess.Popen(['npm', '--loglevel=silent', 'install'])
         s.communicate()
         print("\033[92mFinished\033[0m")
 
 
-def build_bower(repo_dir):
+def build_bower(data, **kwargs):
+    if not data:
+        return
     print("\033[93mRunning bower install...\033[0m")
 
-    with work_in(repo_dir):
+    with work_in(kwargs['repo_dir']):
         s = subprocess.Popen(['./node_modules/bower/bin/bower', 'install'])
         s.communicate()
         print("\033[92mFinished\033[0m")
 
 
-def build_gulp(repo_dir):
+def build_gulp(data, **kwargs):
+    if not data:
+        return
+
     print("\033[93mRunning gulp build...\033[0m")
 
-    with work_in(repo_dir):
+    with work_in(kwargs['repo_dir']):
         s = subprocess.Popen(['./node_modules/gulp/bin/gulp.js', 'build'])
         s.communicate()
         print("\033[92mFinished\033[0m")
 
 
-def build_apache_config(data, venv_name):
+def build_apache_config(data, **kwargs):
     if not data.get('server_path', None):
         raise MissingBuildFileParamException("server_path must be specified under apache_config in the build.yml file")
 
@@ -121,7 +133,19 @@ def build_apache_config(data, venv_name):
 
     print("\033[93mCreating apache conf...\033[0m", end='')
     sys.stdout.flush()
+
     s = subprocess.Popen(['python', '/home/vagrant/django-deployment-script-jenkins/create_httpd_conf.py',
-                          server_path, '--path_to_venv', '{0}/{1}'.format(VENV_DIR, venv_name)])
+                          server_path, '--path_to_venv', '{0}/{1}'.format(VENV_DIR, kwargs['venv'])])
     s.communicate()
+    print("\033[92mFinished\033[0m")
+
+
+def build_arbitrary(data, **kwargs):
+    print("\033[93mRunning arbitrary command...\033[0m")
+    sys.stdout.flush()
+
+    with work_in(kwargs['repo_dir']):
+        s = subprocess.Popen(data)
+        s.communicate()
+
     print("\033[92mFinished\033[0m")
